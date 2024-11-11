5e7db13b27b93d40847885c6947da77e5231afb4
Anushka Mokashi <100767949+AnushkaJMokashi@users.noreply.github.com>
2024-11-11 11:18:32 +0530

    Update ml1_uber.py

--- ml1_uber.py
+++ ml1_uber.py
@@ -1,131 +1,98 @@
 import pandas as pd
- import numpy as np
- import seaborn as sns
- import matplotlib.pyplot as plt
- %matplotlib inline
- df = pd.read_csv('uber.csv')
- df.head()
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+df = pd.read_csv('uber.csv')
+df.head()
+df.drop(['Unnamed: 0', 'key'], axis=1, inplace=True)
+df.head()
+df.info()
+df.passenger_count.value_counts()
+df = df[~ (df.passenger_count == 0) & ~(df.passenger_count == 208)]
+df.shape
+df.isnull().sum()
+## Handle Range of latitude and longitude
+df = df[
+    (df.pickup_longitude < 180) & (df.pickup_longitude > -180) &
+    (df.dropoff_longitude < 180) & (df.dropoff_longitude > -180) &
+    (df.pickup_latitude < 90) & (df.pickup_latitude > -90) &
+    (df.dropoff_latitude < 90) & (df.dropoff_latitude > -90)
+]
+df.shape
+df['pickup_datetime'] = pd.to_datetime(df['pickup_datetime'])
+df['year'] = df['pickup_datetime'].dt.year
+df['month'] = df['pickup_datetime'].dt.month
+df['day'] = df['pickup_datetime'].dt.day
+df['hour'] = df['pickup_datetime'].dt.hour
+df.head()
+df.drop(['pickup_datetime'], axis=1, inplace=True)
+df.head()
+df.duplicated().sum()
+df.drop_duplicates(inplace=True)
+## Haversine distance
 
- df.dropna(inplace=True)
- df.isnull().sum()
+def haversine_distance(lon_1, lon_2, lat_1, lat_2):
+    
+    lon_1, lon_2, lat_1, lat_2 = map(np.radians, [lon_1, lon_2, lat_1, lat_2])  #Degrees to Radians
+    
+    
+    diff_lon = lon_2 - lon_1
+    diff_lat = lat_2 - lat_1
+    
 
-fare_amt = [fare for fare in df.fare_amount if fare>=0]
-len(fare_amt)
-199982
-neg_fare_amt = [fare for fare in df.fare_amount if fare < 0]
-len(neg_fare_amt)
-17
-df.fare_amount = [fare if fare >= 0 else 0 for fare in df.fare_amount]
-fare_amt = [fare for fare in df.fare_amount if fare==0]
-len(fare_amt)
+    km = 2 * 6371 * np.arcsin(
+        np.sqrt(np.sin(diff_lat/2.0)**2 + np.cos(lat_1) * np.cos(lat_2) * np.sin(diff_lon/2.0)**2)
+    )
+    
+    return km
+df['haversine_distance'] = haversine_distance(df['pickup_longitude'],df['dropoff_longitude'], df['pickup_latitude'],df['dropoff_latitude'])
+df.head()
+## Handling Outliers
+numerical_cols = [col for col in df.columns if df[col].dtype != 'object']
 
-long_lati_cols = 
-['pickup_longitude','pickup_latitude','dropoff_longitude','dropoff_latitude']
- for col in long_lati_cols:
-    df[col] = [round(i,4) for i in df[col]]
- df
+for col in numerical_cols:
+    sns.boxplot(df[col])
+    plt.title(f'Boxplot of {col}')
+    plt.show()
+numerical_cols = [col for col in df.columns if df[col].dtype != 'object']
 
+def remove_outliers(df, cols):
+    for col in cols:
+        Q1 = df[col].quantile(0.25)
+        Q3 = df[col].quantile(0.75)
+        IQR = Q3 - Q1
+        lower_bound = Q1 - 1.5 * IQR
+        upper_bound = Q3 + 1.5 * IQR
+        # Filter the DataFrame to remove outliers
+        df = df[(df[col] >= lower_bound) & (df[col] <= upper_bound)]
+    return df
 
-from math import *
- def distance_formula(longitude1,latitude1,longitude2,latitude2):
-    travel_dist = []
-    for pos in range (len(longitude1)):
-        lon1, lan1, lon2, lan2 = map(radians, [longitude1[pos], 
-latitude1[pos], longitude2[pos], latitude2[pos]])
-        dist_lon = lon2 - lon1
-        dist_lan = lan2 - lan1
-        a=sin(dist_lan/2)**2 + cos(lan1) * cos(lan2) * 
-sin(dist_lon/2)**2
-        c = 2 * asin(sqrt(a)) * 6371
-        travel_dist.append(c)
-    return travel_dist
-        
- df['dist_travel_km'] = 
-distance_formula(df.pickup_longitude.to_numpy(),df.pickup_latitude.to_numpy(), df.dropoff_longitude.to_numpy(), df.dropoff_latitude.to_numpy())
-df['dist_travel_km']
-
-
- from datetime import datetime
- df['pickup_datetime'] = pd.to_datetime(df['pickup_datetime'], errors='coerce')
- df
-
- df['pickup_datetime'] = pd.to_datetime(df['pickup_datetime'], format="%m/%d/%Y, %H:%M:%S")
- df.pickup_datetime
-
- df.drop("pickup_datetime",axis=1,inplace=True)
- df
-
-sns.boxplot(x=df['fare_amount'])
-
-
-df.plot(kind = "box", subplots = True, layout = (6,2),figsize=(15,20))
- plt.show() 
-
-def remove_outlier(df1, col) : 
-    Q1 = df1[col].quantile(0.25)
-    Q3 = df1[col].quantile(0.75)
-    IQR = Q3 - Q1
-    lower_bound = Q1 - 1.5 * IQR
-    upper_bound = Q3 + 1.5 * IQR
-    df[col] = np.clip(df1[col], lower_bound, upper_bound)
-    return df1
-
-def treat_outliers(df1, col_list):
-    for c in col_list:
-        df1 = remove_outlier(df1, c)
-    return df1
-df = treat_outliers(df, df.iloc[:,0::])
-df.plot(kind = "box", subplots = True, layout = (6, 2), figsize = (15, 20))
-plt.show()
-corr = df.corr()
-fig, axis = plt.subplots(figsize = (10, 6))
-sns.heatmap(df.corr(), annot = True)
-
+df_clean = remove_outliers(df, numerical_cols)
+df_clean.shape
+for col in numerical_cols:
+    sns.scatterplot(x=df[col], y=df['fare_amount'])
+    plt.title(f'Boxplot of {col}')
+    plt.show()
+sns.scatterplot(x=df['haversine_distance'], y=df['fare_amount'])
+plt.xlim([0, 100])
+df = df[~(df['haversine_distance'] == 0)]
+sns.heatmap(df.corr(),annot=True)
+Y = df.iloc[:,:1]
+X = df.iloc[:,1:]
 from sklearn.model_selection import train_test_split
 
-df_x = df[['pickup_longitude', 'pickup_latitude',
-       'dropoff_longitude', 'dropoff_latitude', 'passenger_count',
-       'dist_travel_km', 'hour', 'day', 'month', 'year', 'dayofweek']]
-
-df_y = df['fare_amount']
-
-x_train, x_test, y_train, y_test = train_test_split(df_x, df_y, test_size = 0.2, random_state = 1)
-
-
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.20, random_state=42)
+## Linear Regression
 from sklearn.linear_model import LinearRegression
 
-model.fit(x_train, y_train)
+lr = LinearRegression()
+lr.fit(X_train, y_train)
+y_pred = lr.predict(X_test)
+from sklearn.metrics import r2_score
 
-y_pred_lin = model.predict(x_test)
-print(y_pred_lin)
-
-
+print('R2 score : ',r2_score(y_test, y_pred))
 from sklearn.ensemble import RandomForestRegressor
-rf = RandomForestRegressor (n_estimators= 100)
-rf.fit(x_train, y_train)
-y_pred_rf = rf.predict(x_test)
-y_pred_rf
+rf = RandomForestRegressor(n_estimators=10)
+rf.fit(X_train, y_train)
+rf_pred = rf.predict(X_test)
 
-
-cols = ['Model', 'RMSE', 'R-Squared']
-result_tabulation = pd.DataFrame(columns = cols)
-
-from sklearn.metrics import r2_score, mean_squared_error
-
-reg_RMSE = np.sqrt(mean_squared_error(y_test, y_pred_lin))
-reg_squared = r2_score(y_test, y_pred_lin)
-
-full_metrics = pd.Series({'Model' : "Linear Regression", 'RMSE' : reg_RMSE, 'R-Squared' : reg_squared})
-
-result_tabulation = result_tabulation._append(full_metrics, ignore_index = True)
-
-result_tabulation
-
-rf_RMSE = np.sqrt(mean_squared_error(y_test, y_pred_rf))
-rf_squared = r2_score(y_test, y_pred_rf)
-
-full_metrics = pd.Series({'Model' : "Random Forest", 'RMSE' : rf_RMSE, 'R-Squared' : rf_squared})
-result_tabulation = result_tabulation._append(full_metrics, ignore_index = True)
-
-result_tabulation
- 
